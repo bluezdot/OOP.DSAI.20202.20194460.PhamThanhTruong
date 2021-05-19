dff748220e0baff96d8d42fcce7588e697c7076c
Truong Pham <thanhtruong27701@gmail.com>
2021-05-19 15:39:26 +0700

    Create Painter

--- GUIProject/src/hust/soict/dsai/javafx/PainterController.java
+++ GUIProject/src/hust/soict/dsai/javafx/PainterController.java
@@ -1,7 +1,11 @@
 package hust.soict.dsai.javafx;
 
+import java.awt.Button;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
@@ -9,14 +13,29 @@ import javafx.scene.shape.Circle;
 
 public class PainterController {
 
+    @FXML
+    private ToggleGroup tgTools;
+
+    @FXML
+    private RadioButton rbEraser;
+
+    @FXML
+    private RadioButton rbPen;
+    
 	@FXML
 	private Pane drawingAreaPane;
 
 	@FXML
     void drawingAreaMouseDragged(MouseEvent event) {
-    	Circle newCircle = new Circle(event.getX(), event.getY(), 4, Color.BLACK); 
-    	drawingAreaPane.getChildren().add(newCircle);
-    }
+		if (rbPen.isSelected()) {
+	    	Circle newCircle = new Circle(event.getX(), event.getY(), 4, Color.BLACK); 
+	    	drawingAreaPane.getChildren().add(newCircle);
+		}
+		else if (rbEraser.isSelected()) {
+			Circle newCircle = new Circle(event.getX(), event.getY(), 4, Color.WHITE); 
+	    	drawingAreaPane.getChildren().add(newCircle);
+		}
+	}
 
 	@FXML
 	void clearButtonPressed(ActionEvent event) {
@@ -25,3 +44,5 @@ public class PainterController {
 	}
 
 }
+
+
