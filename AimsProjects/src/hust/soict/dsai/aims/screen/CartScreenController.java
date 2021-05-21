4dfb356b6b1821da669f3a0b84e48ed2b884ccd2
Truong Pham <thanhtruong27701@gmail.com>
2021-05-21 17:29:57 +0700

    Create filter for table

--- AimsProjects/src/hust/soict/dsai/aims/screen/CartScreenController.java
+++ AimsProjects/src/hust/soict/dsai/aims/screen/CartScreenController.java
@@ -6,16 +6,21 @@ import hust.soict.dsai.aims.media.Media;
 import hust.soict.dsai.aims.media.Playable;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.control.cell.PropertyValueFactory;
 
 public class CartScreenController {
 	private Cart cart;
+//	private Store store;
 	
     @FXML
     private Button btnPlay;
@@ -38,9 +43,19 @@ public class CartScreenController {
 	@FXML
 	private TableColumn<Media, Float> colMediaCost;
 	
+	@FXML
+	private TextField tfFilter;
+	
+	@FXML
+	private RadioButton radioBtnFilterId;
+	
+	@FXML
+	private RadioButton radioBtnFilterTitle;
+	
 	public CartScreenController(Cart cart) {
 		super();
 		this.cart = cart;
+//		this.store = store;
 	}
 	
 	@FXML
@@ -48,7 +63,10 @@ public class CartScreenController {
 		colMediaTitle.setCellValueFactory(new PropertyValueFactory<Media, String>("title"));
 		colMediaCategory.setCellValueFactory(new PropertyValueFactory<Media, String>("category"));
 		colMediaCost.setCellValueFactory(new PropertyValueFactory<Media, Float>("cost"));
-		tblMedia.setItems(this.cart.getItemsOrdered());
+		
+		//get item in listMedia
+		ObservableList<Media> listMedia = this.cart.getItemsOrdered();
+		tblMedia.setItems(listMedia);
 		
 		btnPlay.setVisible(false);
 		btnRemove.setVisible(false);
@@ -63,6 +81,14 @@ public class CartScreenController {
 						}
 					}
 				});
+		
+		// Add filter
+		tfFilter.textProperty().addListener(new ChangeListener<String>(){
+			@Override
+			public void changed(ObservableValue <? extends String> observable, String oldValue, String newValue) {
+				showFilteredMedia(newValue);
+			}
+		});
 	}
 	
 	void updateButtonBar(Media media) {
@@ -81,5 +107,27 @@ public class CartScreenController {
 		Media media = tblMedia.getSelectionModel().getSelectedItem();
 		cart.removeMedia(media);
 	}
+	
+	void showFilteredMedia(String newValue) {
+        FilteredList<Media> filterData = new FilteredList<>(this.cart.getItemsOrdered(), p -> true);
+        filterData.setPredicate(media -> {
+            if (newValue == null || newValue.isEmpty()) {
+                return true;
+            }
+            String newValueLower = newValue.toLowerCase();
+
+            if (radioBtnFilterTitle.isSelected()) {
+            	return media.getTitle().toLowerCase().contains(newValueLower);
+            	
+                
+            } else if (radioBtnFilterId.isSelected()) {
+            	int idSearch = Integer.parseInt(newValueLower);
+            	return media.getId() == idSearch;
+            }
+            return false;
+        });
+
+        tblMedia.setItems(filterData);
+    }
 }
 
